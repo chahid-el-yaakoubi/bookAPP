69dc2a3e9b908c5a384df3cf6473ea7b72554530
axistay <axistay01@gmail.com>
2025-01-21 19:54:35 +0100

    fix on admin 21/01/2025

--- adminApp/vite.config.js
+++ adminApp/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { BASE_URL } from './src/pages/utils/apiConfig'; // Import the value
 
-export default defineConfig({
+export default {
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: BASE_URL || "http://localhost:18049", // Use imported BASE_URL or default to localhost
+        target: "https://axistay-backend.onrender.com" || " http://localhost:18049",  // Default to localhost for dev
         changeOrigin: true,
         secure: false,
       },
@@ -19,4 +18,4 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-});
+};

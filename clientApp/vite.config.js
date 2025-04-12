598830ec15322427fca08e60955de99dca78f7bd
axistay <axistay01@gmail.com>
2025-04-12 22:40:28 +0200

    05/04/2025

--- clientApp/vite.config.js
+++ clientApp/vite.config.js
@@ -1,7 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 const apiUrl = import.meta.env.VITE_API_URL;
+
+console.log('API URL:', apiUrl);  // Add this line to verify if it's loaded correctly
 
 export default defineConfig({
   plugins: [react()],

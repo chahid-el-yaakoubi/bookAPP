cb8c8eba1a201744c7ae3e7d3ba078964167d57a
axistay <axistay01@gmail.com>
2025-04-12 22:47:18 +0200

    05/04/2025

--- clientApp/vite.config.js
+++ clientApp/vite.config.js
@@ -1,19 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const apiUrl = import.meta.env.VITE_API_URL;
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext', // Set output to 'esm' format for compatibility with import.meta
-  },
   css: {
     postcss: './postcss.config.cjs', // Explicitly point to the PostCSS config file
   },
   server: {
     proxy: {
-      '/api': apiUrl, // Replace with your backend server URL
+      '/api': 'http://localhost:18099', // Replace with your backend server URL
     },
     hmr: {
       overlay: true,
